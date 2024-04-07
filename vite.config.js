a7f656c0da616ac72b4b4ecf344f257c369ecaa0
keniwane <ken@skillyah.com>
2024-04-07 11:27:12 -0700

    base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/os-recruiter-frontend/',
+  base: './os-recruiter-frontend/',
 });
